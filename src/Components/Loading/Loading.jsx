5d0a9ce31c7eca13024610a328da744aaeed0b1e
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2022-04-16 08:56:13 +0530

    add: loading, services, actionTypes

---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="flex-row-center-center">
+            <img src="/Assets/loading.svg" alt="Loading" />
+        </div>
+    );
+}

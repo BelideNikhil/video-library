79836db527bf1f6ab479135de0e991b3ce853ddb
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2022-04-17 16:16:51 +0530

    refactor; video-card, add: menu, playlist-modal

---
+++ src/Components/PlaylistModal/PlaylistModal.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { useAuth, usePlaylist } from "../../Hooks";
+import { Loading } from "../index";
+import "./PlaylistModal.css";
+
+export default function PlaylistModal({ setPlaylistModal, video }) {
+    const [newPlaylist, setNewPlaylist] = useState({ text: "", toggle: false });
+    const {
+        playlistState: { playlists, isLoading },
+        createPlaylistHandler,
+        addToPlaylistHandler,
+        removeFromPlaylistHandler,
+    } = usePlaylist();
+
+    const {
+        authState: { token },
+    } = useAuth();
+
+    function playlistFormHandler(e) {
+        e.preventDefault();
+        if (newPlaylist.text.trim() !== "") {
+            createPlaylistHandler({ video, newPlaylist: newPlaylist.text, token });
+            setNewPlaylist({ text: "", toggle: false });
+        }
+    }
+
+    return (
+        <div className="playlist-modal-wrapper flex-row-center-center">
+            <div className="playlist-content my-8 pa-24">
+                <div className="flex-row-spc-btw">
+                    <h4>Save To</h4>
+                    <button className="btn-icon btn-icon-sm close-modal-btn" onClick={() => setPlaylistModal(false)}>
+                        <i className="fas fa-times"></i>
+                    </button>
+                </div>
+                {isLoading ? (
+                    <Loading className="loading-small" />
+                ) : (
+                    <ul className="flex-clmn-spc-btw playlist-modal-ul ">
+                        {playlists?.length ? (
+                            playlists.map((playlist) => {
+                                const presentInPlaylist = playlist.videos.find((each) => each._id === video._id);
+                                return (
+                                    <li key={playlist._id} className="form-control">
+                                        <label className="form-label flex-row-start-center">
+                                            <input
+                                                type="checkbox"
+                                                className="form-input mr-8"
+                                                checked={presentInPlaylist ? true : false}
+                                                onChange={() =>
+                                                    presentInPlaylist
+                                                        ? removeFromPlaylistHandler({ video, token, playlist })
+                                                        : addToPlaylistHandler({ video, token, playlist })
+                                                }
+                                            />
+                                            {playlist.title}
+                                        </label>
+                                    </li>
+                                );
+                            })
+                        ) : (
+                            <h4 className="txt-center">No playlists available</h4>
+                        )}
+                    </ul>
+                )}
+                {newPlaylist.toggle ? (
+                    <form className="user-form my-8" onSubmit={playlistFormHandler}>
+                        <input
+                            type="text"
+                            className="form-input my-4"
+                            placeholder="Create playlist..."
+                            onChange={(e) => setNewPlaylist((prev) => ({ ...prev, text: e.target.value }))}
+                            value={newPlaylist.text}
+                        />
+                        <button className="btn btn-solid-primary create-playlist-btn" type="submit">
+                            Create
+                        </button>
+                    </form>
+                ) : (
+                    <div>
+                        <button
+                            className="btn btn-solid-primary create-playlist-btn"
+                            onClick={() => setNewPlaylist((prev) => ({ ...prev, toggle: true }))}
+                        >
+                            Create Playlist
+                        </button>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
--- src/Components/VideoCard/VideoCard.jsx
+++ src/Components/VideoCard/VideoCard.jsx
@@ -1,9 +1,25 @@
 import "./VideoCard.css";
 import { getThumbnail, getCreatorImg } from "../../Utils";
+import { ThreeDotMenu } from "../index";
+import { useState, useRef, useEffect } from "react";
+
 export default function VideoCard({ video }) {
-    const { _id, title, creator, creatorId, views } = video;
+    const [showMenu, setShowMenu] = useState(false);
+    const { _id, title, creator, creatorId } = video;
+    const videoCardRef = useRef(null);
+
+    useEffect(() => {
+        const checkIfClickedOutside = (e) => {
+            if (showMenu && videoCardRef.current && !videoCardRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+        document.addEventListener("mousedown", checkIfClickedOutside);
+        return () => document.removeEventListener("mousedown", checkIfClickedOutside);
+    }, [showMenu]);
+
     return (
-        <div className="video-card pointer">
+        <div className="video-card pointer" ref={videoCardRef}>
             <img src={getThumbnail(_id)} alt={title} className="thumbnail-img" />
             <div className="video-content-wrapper mt-6">
                 <div className="video-content">
@@ -13,10 +29,11 @@ export default function VideoCard({ video }) {
                         <div className="creator-tag">{creator}</div>
                     </div>
                 </div>
-                <div>
-                    <button className="video-menu-btn pointer">
+                <div style={{ position: "relative" }}>
+                    <button className="video-menu-btn pointer" onClick={() => setShowMenu((prev) => !prev)}>
                         <span className="material-icons-outlined">more_vert</span>
                     </button>
+                    {showMenu ? <ThreeDotMenu video={video} /> : null}
                 </div>
             </div>
         </div>
